f7c6cd6e49bf58266eae876c698d9f3bd3cefa13
yuratouch <yu.touch@icloud.com>
2021-04-23 16:30:52 +0300

    MAG-48: Create requireJS module that returns a UI component

---
+++ app/design/frontend/Clothes/touch/web/js/task48.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define(['uiComponent'], function (Component) {
+    'use strict';
+
+    return Component.extend({
+        initialize: function () {
+            console.log('MAG-48 initialize')
+        }
+    })
+});
